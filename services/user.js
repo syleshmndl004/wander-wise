646e95d50561cbdd21b3366cf467fdcd5abd119d
user.name "Sailesh Mandal <user.email "saileshkumar2061@gmail.com>
2026-07-02 18:03:07 +0545

    Refactor: enhance user creation function to exclude password from response

--- services/user.js
+++ services/user.js
@@ -1,6 +1,9 @@
 // Service function for creating a new user
 import User from '../models/user.js';
 
-export const create = async (data) => {
-    return User.create(data);
+export const create = async (data) => { // Create a new user in the database
+    const user = await User.create(data);// Create a new user in the database
+    const { password , ...userWithoutPassword } = user.toobject();// Remove the password field from the user object before returning it
+    return userWithoutPassword;// Return the user object without the password field
 }
+  
